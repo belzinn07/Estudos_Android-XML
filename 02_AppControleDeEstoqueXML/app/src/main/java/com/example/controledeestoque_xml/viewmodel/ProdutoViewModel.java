c49e2a2ab6f2b90fefae1a64d53480d48c5c937b
Belmiro Neto <netobelmiro1@gmail.com>
2025-10-19 19:57:57 -0300

    Update ProdutoViewModel.java
    - Melhoria no codigo e implementação da biblioteca MediatorLiveData

--- 02_AppControleDeEstoqueXML/app/src/main/java/com/example/controledeestoque_xml/viewmodel/ProdutoViewModel.java
+++ 02_AppControleDeEstoqueXML/app/src/main/java/com/example/controledeestoque_xml/viewmodel/ProdutoViewModel.java
@@ -1,10 +1,13 @@
 package com.example.controledeestoque_xml.viewmodel;
 
 import android.app.Application;
+import android.util.Log;
 
 import androidx.annotation.NonNull;
 import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MediatorLiveData;
+import androidx.lifecycle.Observer;
 
 import com.example.controledeestoque_xml.model.Produto;
 import com.example.controledeestoque_xml.model.ProdutoRepository;
@@ -15,14 +18,33 @@ public class ProdutoViewModel extends AndroidViewModel{
     private ProdutoRepository produtoRepository;
     private LiveData<List<Produto>> todosProdutos;
 
+    private MediatorLiveData<Double> valorTotalEstoque = new MediatorLiveData<>();
+
     public ProdutoViewModel(@NonNull Application application){
         super(application);
         produtoRepository = new ProdutoRepository(application);
         todosProdutos = produtoRepository.getTodosProdutos();
+
+        valorTotalEstoque.addSource(todosProdutos, new Observer<List<Produto>>(){
+            @Override
+            public void onChanged(List<Produto> produtos) {
+                double total = 0;
+                if(produtos != null){
+                    for (Produto produto : produtos){
+                        total += produto.getQuantidade() * produto.getPrecoUnitario();
+                    }
+                }
+                valorTotalEstoque.setValue(total);
+            }
+        });
     }
 
     public LiveData<List<Produto>> getTodosProdutos() {
         return todosProdutos;
+    }
+
+    public LiveData<Double> getValorTotalEstoque() {
+        return valorTotalEstoque;
     }
 
     public void adicionarProduto(Produto produto){
@@ -36,5 +58,17 @@ public class ProdutoViewModel extends AndroidViewModel{
     public void deletarProduto(Produto produto){
         produtoRepository.deletarProduto(produto);
     }
+/*
+    public boolean validarCampos(){
+        List<Produto> lista = todosProdutos.getValue();
 
+        for (Produto produto : lista){
+            if (produto.getNome().isEmpty() || produto.getPrecoUnitario() <= 0 || produto.getQuantidade() <= 0){
+                Log.e("ProdutoViewModel","Preencha todos os campos corretamente");
+                return false;
+            }
+        }
+        return true;
+    }
+*/
 }
