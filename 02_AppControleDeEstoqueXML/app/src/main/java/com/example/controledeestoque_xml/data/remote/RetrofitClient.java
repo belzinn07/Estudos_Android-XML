23e5e22249e3a44edfc3f970179fbf73d3f8da1b
Belmiro Neto <netobelmiro1@gmail.com>
2025-12-16 14:56:12 -0300

    Adição do ícone do app

--- 02_AppControleDeEstoqueXML/app/src/main/java/com/example/controledeestoque_xml/data/remote/RetrofitClient.java
+++ 02_AppControleDeEstoqueXML/app/src/main/java/com/example/controledeestoque_xml/data/remote/RetrofitClient.java
@@ -9,8 +9,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    private static final String BASE_URL = "http://192.168.0.109:8080";
-
+   //private static final String BASE_URL = "http://192.168.0.109:8080";
+    private static final String BASE_URL = "http://192.168.0.113:8080/";
      //private static final String BASE_URL = "http://10.0.2.2:8080";
     private static Retrofit retrofit;
 
